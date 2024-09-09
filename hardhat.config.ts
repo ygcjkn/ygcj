fab6a44f96fe30a4bb7eae0fe990bf7829c49860
root <ygcjkn@outlook.com>
2024-09-09 12:56:49 +0000

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,83 @@
+require("@nomicfoundation/hardhat-toolbox");
+// Remember to use the private key of a testing account
+// For better security practices, it's recommended to use npm i dotenv for storing secret variables
+
+module.exports = {
+  defaultNetwork: "swisstronik",
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.16",
+      },
+      {
+        version: "0.8.0",
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      {
+        version: "0.8.24",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`ef85509376e1537aeb0ee530117fd3533ed3d3d7bcd3999eef0c5527b73973e6`],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      swisstronik: "ANY_STRING_WILL_DO",
+    },
+    customChains: [
+      {
+        network: "swisstronik",
+        chainId: 1291,
+        urls: {
+          apiURL: "https://explorer-evm.testnet.swisstronik.com/api",
+          browserURL: "https://explorer-evm.testnet.swisstronik.com",
+        },
+      },
+    ],
+  },
+};
